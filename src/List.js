547dc8d7a1da8bb5442806e5717c4de9ccb7a2d6
RAHULPATHAK <rahulpathak@costacloud.com>
2022-06-21 18:39:24 +0530

    chnges

--- src/List.js
+++ src/List.js
@@ -22,7 +22,7 @@
 //   };
 
 //   const [isExpand, setExpand] = useState(false);
-  
+
 //   return (
 //     <div className="list_container">
 //       <h1 className="heading_list">Running Process List</h1>
@@ -46,7 +46,7 @@
 //                     className="tableCell_"
 //                     // onClick={() => goToProgressPage(data)}
 //                     style={{ cursor: "pointer" }}
-                 
+
 //                   >
 //                     {id + 1}
 //                   </TableCell>
@@ -64,76 +64,33 @@
 
 // export default List;
 
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import IconButton from '@material-ui/core/IconButton';
-// import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
-// import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import IconButton from "@material-ui/core/IconButton";
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+import data from "./data.json";
+import ProgressPage from "./ProgressPage";
+import Cookies from "js-cookie";
+import { useNavigate } from "react-router-dom";
 
 import "./Assets/CSS/List.css";
 
 const useStyles = makeStyles({
   root: {
-    width: '100%',
-    overflowX: 'auto'
+    width: "100%",
+    overflowX: "auto",
   },
   table: {
-    minWidth: 650
-  }
+    minWidth: 550,
+  },
 });
-
-function createData(name, calories, fat, carbs, protein, detail) {
-  return { name, calories, fat, carbs, protein, detail };
-}
-
-const rows = [
-  createData(
-    'Frozen yoghurt',
-    159,
-    6.0,
-    24,
-    4.0,
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
-  ),
-  createData(
-    'Ice cream sandwich',
-    237,
-    9.0,
-    37,
-    4.3,
-    'Maecenas rutrum urna vel lacus viverra, id ultrices dui rutrum'
-  ),
-  createData(
-    'Eclair',
-    262,
-    16.0,
-    24,
-    6.0,
-    'Morbi congue gravida nunc, eu cursus felis vulputate id'
-  ),
-  createData(
-    'Cupcake',
-    305,
-    3.7,
-    67,
-    4.3,
-    'Vestibulum efficitur, ipsum consectetur finibus maximus, ligula dolor vehicula ex, sed viverra nulla mauris id purus'
-  ),
-  createData(
-    'Gingerbread',
-    356,
-    16.0,
-    49,
-    3.9,
-    ' Suspendisse vehicula eu libero eget viverra'
-  )
-];
 
 const ExpandableTableRow = ({ children, expandComponent, ...otherProps }) => {
   const [isExpanded, setIsExpanded] = React.useState(false);
@@ -143,7 +100,7 @@ const ExpandableTableRow = ({ children, expandComponent, ...otherProps }) => {
       <TableRow {...otherProps}>
         <TableCell padding="checkbox">
           <IconButton onClick={() => setIsExpanded(!isExpanded)}>
-            {/* {isExpanded ? <ArrowUpwardIcon /> : <ArrowDownwardIcon />} */}
+            {isExpanded ? <ArrowUpwardIcon /> : <ArrowDownwardIcon />}
           </IconButton>
         </TableCell>
         {children}
@@ -158,35 +115,39 @@ const ExpandableTableRow = ({ children, expandComponent, ...otherProps }) => {
   );
 };
 
-export default function SimpleTable() {
+const List = () => {
+  let navigate = useNavigate();
+  const goToProgressPage = (data) => {
+    Cookies.set("id", data.id);
+    let path = "/progress/";
+    navigate(path);
+  };
   const classes = useStyles();
 
   return (
     <Paper className={classes.root}>
+      <h1 className="heading_list">Candidate Status</h1>
       <Table className={classes.table} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell padding="checkbox" />
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell />
+            <TableCell align="center">Id</TableCell>
+            <TableCell align="center">Time</TableCell>
+            <TableCell align="center">Status</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map(row => (
+          {data.map((data, id) => (
             <ExpandableTableRow
-              key={row.name}
-              expandComponent={<TableCell colSpan="5">{row.detail}</TableCell>}
+              expandComponent={
+                <TableCell colSpan="5" onClick={() => goToProgressPage(data)}>
+                  <ProgressPage />
+                </TableCell>
+              }
             >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="center">{id + 1}</TableCell>
+              <TableCell align="center">{data.time}</TableCell>
+              <TableCell align="center">{data.status}</TableCell>
             </ExpandableTableRow>
           ))}
         </TableBody>
@@ -194,3 +155,4 @@ export default function SimpleTable() {
     </Paper>
   );
 }
+export default List;
